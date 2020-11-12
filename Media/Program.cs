ee40aa6161ded8f20c7e377b9095b89445f4f3f2
Lucas <lucasimionato1910@hotmail.com>
2020-11-12 17:16:29 -0200

    Media

---
+++ Media/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a primeira nota");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota");
+            double nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota");
+            double nota3 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3) / 3;
+            string situacao = "";
+
+            Console.WriteLine("Média: " + media);
+            if (media < 7) {
+                situacao = "Reprovado";
+            } else {
+                situacao = "Aprovado";
+            }
+            Console.WriteLine(situacao);
+        }
+    }
+}
