0e1caff0203016303df12d66f2639851783e8f44
Lucas <lucasimionato1910@hotmail.com>
2020-11-12 17:33:15 -0200

    Semaforo

---
+++ Semáforo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Semáforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Responda com letras minusculas sem espaços");
+            Console.WriteLine("Qual cor do farol está acesa?");
+            string resposta = Console.ReadLine();
+            if (resposta == "verde") {
+                Console.WriteLine("Siga em frente");
+            } else if (resposta == "amarelo") {
+                Console.WriteLine("Preste atenção");
+            } else if (resposta == "vermelho") {
+                Console.WriteLine("Pare!");
+            } else {
+                Console.WriteLine("Resposta inválida");
+            }
+        }
+    }
+}
