6b8f486a5a10e4c5188bb339bd03f24564dfa9a2
DEV <dev@info.local>
2020-11-13 16:29:38 -0300

    exemplo adicionado

---
+++ ExerciciosCondicionais/Exemplo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exclui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para participar do time o aluno precisa de nota maior ou igual a 80 e faltas menor ou igual a 10% dias letivos
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Você quer participar?");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("");
+            string a = Console.ReadLine();
+            Console.WriteLine("---------------------------------------------");
+            if (a == "sim") {
+                Console.WriteLine("Legal, vamos ver se você pode");
+                Console.WriteLine("Manda sua media");
+                Console.WriteLine("---------------------------------------------");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Agora as faltas");
+                Console.WriteLine("---------------------------------------------");
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------");
+                if (b >=50 && c <=10){
+                    Console.WriteLine("Parabens, voce passou");
+                } else {
+                    Console.WriteLine("Falte menos e tire boas notas");
+                    Console.WriteLine("Passar bem");
+                }
+            } else {
+                Console.WriteLine("Passar bem");
+            }
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
